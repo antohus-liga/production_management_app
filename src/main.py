0898cc19f1c66a599462991899b95f9e982cc65e
antohus-liga <antonioslotylo0904@gmail.com>
2025-08-29 19:32:14 +0100

    Database and logging setup

---
+++ src/main.py
@@ -0,0 +1,29 @@
+import logging
+import os
+import sys
+
+from PySide6.QtWidgets import QApplication, QWidget
+from sqlalchemy import create_engine
+
+app = QApplication(sys.argv)
+
+data_path = os.path.join(
+    os.path.dirname(__file__),
+    "..",
+    "data",
+)
+db_path = os.path.join(data_path, "stock_info.db")
+logs_path = os.path.join(data_path, "db_logs.log")
+
+logging.basicConfig(
+    filename=logs_path,
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+)
+logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+engine = create_engine(f"sqlite:///{db_path}")
+
+widget = QWidget()
+widget.show()
+
+app.exec()
