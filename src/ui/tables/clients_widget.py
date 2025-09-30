46b8161af394fe974627e5e05684504411b70c55
antohus-liga <antonioslotylo0904@gmail.com>
2025-09-30 12:50:54 +0100

    Added data insertion (tested)

--- src/ui/tables/clients_widget.py
+++ src/ui/tables/clients_widget.py
@@ -1,5 +1,6 @@
-from PySide6.QtWidgets import (QHeaderView, QPushButton, QSizePolicy,
-                               QTableView, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QHBoxLayout, QHeaderView, QLineEdit,
+                               QPushButton, QSizePolicy, QTableView,
+                               QVBoxLayout, QWidget)
 
 from ui.tables.clients_model import ClientsTableModel
 
@@ -24,13 +25,23 @@ class ClientsWidget(QWidget):
         go_back_btn = QPushButton("Go back")
         go_back_btn.clicked.connect(lambda: self.stack.setCurrentIndex(0))
 
-        layout = QVBoxLayout()
-
         size = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
         size.setHorizontalStretch(1)
         self.table_view.setSizePolicy(size)
 
-        layout.addWidget(self.table_view)
+        confirm_btn = QPushButton("confirm changes")
+        add_btn = QPushButton("insert")
+
+        add_btn.clicked.connect(lambda: self.model.insertRow(self.model.rowCount()))
+        confirm_btn.clicked.connect(lambda: self.model.submitAll())
+
+        util_layout = QHBoxLayout()
+        util_layout.addWidget(add_btn)
+        util_layout.addWidget(confirm_btn)
+
+        layout = QVBoxLayout()
         layout.addWidget(go_back_btn)
+        layout.addWidget(self.table_view)
+        layout.addLayout(util_layout)
 
         self.setLayout(layout)
